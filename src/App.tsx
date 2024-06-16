856b1811087180c7f351636ad8b29b741fec8c34
hsoaresbianca <biasoares.bianca@gmail.com>
2024-06-15 21:58:35 -0300

    first commit

--- src/App.tsx
+++ src/App.tsx
@@ -1,25 +1,37 @@
-import React from 'react';
-import logo from './logo.svg';
-import './App.css';
+import { Layout } from "./components/Layout";
+import styled from "styled-components";
+import {
+  Box,
+  Button,
+  Center,
+  ChakraProvider,
+  Input
+} from '@chakra-ui/react'
+
+import { login } from "./services/login";
+
 
 function App() {
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.tsx</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
+    <ChakraProvider>
+      <Box minHeight='100vh' backgroundColor='#9413dc' padding='25px'>
+        <Box backgroundColor='#fff' borderRadius='25px' padding='15px'>
+          <Center>
+            <h1>Faça o login</h1>
+          </Center>
+          <Input placeholder="email" />
+          <Input placeholder="password" />
+          <Center>
+            <Button onClick={login} colorScheme='teal' size='md' width='100%' marginTop='8px'>
+              Button
+            </Button>
+          </Center>
+
+        </Box>
+
+      </Box>
+
+    </ChakraProvider>
   );
 }
 
---
+++ src/services/login.test.tsx
@@ -0,0 +1,13 @@
+import { login } from "./login"
+
+describe('login', () => {
+
+    const mockAlert = jest.fn()
+    window.alert = mockAlert
+
+    it('deve exibir um alert com boas vindas', () => {
+        login()
+        expect(mockAlert).toBeCalled()
+    })
+}
+)
