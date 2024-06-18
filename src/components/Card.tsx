6d06fa303ff1349e0f977b77132e5521af84c15a
hsoaresbianca <biasoares.bianca@gmail.com>
2024-06-18 16:31:13 -0300

    updating card component

--- src/components/Card.tsx
+++ src/components/Card.tsx
@@ -1,15 +1,31 @@
-interface ICard {
-  id: number,
-  paragraph: string,
-  details: string
-}
+import { 
+  Center,
+  ChakraProvider,
+  Input,
+  Box
+} from '@chakra-ui/react'
 
-export const Card = ({ id, paragraph, details }: ICard) => {
+import { Header } from './Header'
+import { EnterButton } from './EnterButton'
+
+export const Card = (): any => {
   return(
-    <div>
-      <h1>Card {id}</h1>
-      <p>{paragraph}</p>
-      <p>{details}</p>
-    </div>
+    <ChakraProvider>
+      <Center background='orange' fontSize='x-large'>
+        <Header/>
+      </Center>
+      <Box minHeight='100vh' backgroundColor='#9413dc' padding='25px'>
+        <Box backgroundColor='#FFFFFF' borderRadius='25px' padding='15px' >
+          <Center>
+            <h1>Faça o login</h1>
+          </Center>
+          <Input placeholder="email" />
+          <Input placeholder="password" />
+          <Center>
+            <EnterButton/>
+          </Center>
+        </Box>
+      </Box>
+    </ChakraProvider>
   )
 }
